f1eaabb12e1c1e30065dd5a59c34ece5ee5ba396
Javis <jelvismbah18@gmail.com>
2024-05-18 13:36:14 +0100

    Update view.dart

--- lib/screens/home/home_screen/profile_page/view.dart
+++ lib/screens/home/home_screen/profile_page/view.dart
@@ -5,11 +5,11 @@ import 'package:crosspay/controllers/user_controller.dart';
 import 'logic.dart';
 
 class ProfilePagePage extends StatelessWidget {
-  ProfilePagePage({Key? key}) : super(key: key);
+  ProfilePagePage({super.key});
 
   final logic = Get.put(ProfilePageLogic());
   final state = Get.find<ProfilePageLogic>().state;
-  var userController = Get.put(UserController());
+  final userController = Get.put(UserController());
 
   @override
   Widget build(BuildContext context) {
