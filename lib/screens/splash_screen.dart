670c8abc3957acec583b9d1babd7225991fd59c8
Javis <jelvismbah18@gmail.com>
2024-05-18 13:32:48 +0100

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -3,7 +3,7 @@ import 'package:crosspay/theme/annotated_system_ui.dart';
 import 'package:crosspay/theme/colors.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+  const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
