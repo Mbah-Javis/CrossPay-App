4e8cecbe3357025ef6f6fbd1156c4163ab21d60e
Javis <jelvismbah18@gmail.com>
2024-05-18 13:35:44 +0100

    Update state.dart

--- lib/screens/home/home_screen/state.dart
+++ lib/screens/home/home_screen/state.dart
@@ -1,23 +1,22 @@
-import 'package:crosspay/models/crosspay_user.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'home_page/view.dart';
 import 'favorites_page/view.dart';
 import 'profile_page/view.dart';
-import 'package:dot_navigation_bar/src/DotNavigationBarItem.dart';
+import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 
 class HomeScreenState {
   HomeScreenState() {}
 
   List<DotNavigationBarItem> navItems = [
     DotNavigationBarItem(
-      icon: Icon(Icons.home_rounded),
+      icon: const Icon(Icons.home_rounded),
     ),
     DotNavigationBarItem(
-      icon: Icon(Icons.favorite_rounded),
+      icon: const Icon(Icons.favorite_rounded),
     ),
     DotNavigationBarItem(
-      icon: Icon(Icons.person),
+      icon: const Icon(Icons.person),
     ),
   ];
 
