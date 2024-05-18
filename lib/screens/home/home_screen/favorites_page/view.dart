1b98832e3531958204149fba64394ea7d82488da
Javis <jelvismbah18@gmail.com>
2024-05-18 13:38:20 +0100

    Update view.dart

--- lib/screens/home/home_screen/favorites_page/view.dart
+++ lib/screens/home/home_screen/favorites_page/view.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 import 'logic.dart';
 
 class FavoritesPagePage extends StatelessWidget {
-  FavoritesPagePage({Key? key}) : super(key: key);
+  FavoritesPagePage({super.key});
 
   final logic = Get.put(FavoritesPageLogic());
   final state = Get.find<FavoritesPageLogic>().state;
