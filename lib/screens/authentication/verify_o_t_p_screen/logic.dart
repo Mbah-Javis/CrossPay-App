c223b5a31a41e17ba43cf8c01158f1b4431e306a
Javis <jelvismbah18@gmail.com>
2024-05-18 13:41:23 +0100

    Update logic.dart

--- lib/screens/authentication/verify_o_t_p_screen/logic.dart
+++ lib/screens/authentication/verify_o_t_p_screen/logic.dart
@@ -4,7 +4,6 @@ import 'package:crosspay/screens/home/home_screen/view.dart';
 import 'package:crosspay/utils/cross_pay_navigator.dart';
 import 'package:crosspay/api/remote/firebase_auth_service.dart';
 import 'package:crosspay/controllers/user_controller.dart';
-import 'package:flutter/material.dart';
 import 'package:crosspay/models/crosspay_user.dart';
 import 'package:crosspay/utils/c_p_alerts.dart';
 
